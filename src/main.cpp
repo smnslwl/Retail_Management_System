bdc09bb4906ad0f9d388a6fc7a7aa4bcb9700dbc
Samin Silwal <smnslwl@gmail.com>
2019-05-23 23:02:13 +0545

    Bump version to 0.0.4

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
         // Initialize the application
         QApplication application(argc, argv);
         QApplication::setApplicationName(QCoreApplication::tr("Retail Management System"));
-        QApplication::setApplicationVersion("0.0.3");
+        QApplication::setApplicationVersion("0.0.4");
 
         // Display the login window
         LoginWindow loginWindow;
